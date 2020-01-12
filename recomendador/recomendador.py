70f0a8470a6489c901a3b656d53721a4aa6cbb29
bobboyms <bobboyms@gmail.com>
2020-01-11 20:29:50 -0400

    update

--- recomendador/recomendador.py
+++ recomendador/recomendador.py
@@ -4,16 +4,34 @@ import datetime
 client = MongoClient('mongodb://bobboyms:cpqd118@ds125381.mlab.com:25381/bolsa?retryWrites=false')
 db = client.bolsa
 
+data_inicial = datetime.datetime.strptime("01/12/2019", "%d/%m/%Y")
+data_final = datetime.datetime.strptime("29/12/2019", "%d/%m/%Y")
+
+#código da ação
+
+results = db.cotacao.find({
+        "dateobj":{"$gte":data_inicial, "$lte":data_final},
+        "open":{"$gt":0},
+        "volume":{"$gt":15000}
+        },        
+{"codigo":1})
+
+lista = []
+for r in results:
+    lista.append(r["codigo"])
+
+setemp = set(lista)
+papeis = list(setemp)
+
 indicacoes = db.empresa_detalhe.find({
     "indicadores_fundamentalistas.roe" : {"$gt":0},
     "indicadores_fundamentalistas.p_l" : {"$gt":0},
-    "mercado.ano_ult_balanco_processado" : "2019"
+    "mercado.ano_ult_balanco_processado" : "2019",
+    "empresa.codigo":{"$in":papeis}
 },
     {"empresa.codigo":1,"indicadores_fundamentalistas.roe":1,"indicadores_fundamentalistas.p_l":1}).sort(
     [("indicadores_fundamentalistas.roe", -1),("indicadores_fundamentalistas.p_l" , 1)])
 #pl = db.empresa_detalhe.find({}, {"indicadores_fundamentalistas.p_l":1}).sort({"indicadores_fundamentalistas.p_l": -1})
-
-print(roe)
 
 for ind in indicacoes:
     print(ind)
